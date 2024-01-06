ee483b72a4bf34f426647e979c44bc63dff38032
Tommi Koivula <tommi.koivula93@gmail.com>
2024-01-06 19:00:48 +0200

    replace date2 in isholiday

--- src/prices.mjs
+++ src/prices.mjs
@@ -89,10 +89,10 @@ function createApp(database) {
     for (let row of holidays) {
       let holiday = new Date(row.holiday);
       if (
-        date &&
-        date.getFullYear() === holiday.getFullYear() &&
-        date.getMonth() === holiday.getMonth() &&
-        date.getDate() === holiday.getDate()
+        date2 &&
+        date2.year === holiday.getFullYear() &&
+        date2.month - 1=== holiday.getMonth() &&
+        date2.day === holiday.getDate()
       ) {
         return true;
       }
